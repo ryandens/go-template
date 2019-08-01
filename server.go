95783b9a753a214662fb75fa62c8daa24ce454f8
Ryan Dens <ryan.dens@contrastsecurity.com>
2019-08-01 13:44:18 -0400

    :lock: resolve unhandled errors triggering gosec failure

--- server.go
+++ server.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home")
+	_, err := fmt.Fprintf(w, "Home")
+	if err != nil {
+		log.Fatalf("Problem writing response with error %v", err)
+	}
 }
 
 func main() {
 	http.HandleFunc("/", HomeHandler)
-	http.ListenAndServe(":8080", nil)
+	log.Print("Listening on localhost")
+	// log the error
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }

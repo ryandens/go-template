8ecee7d63c732a20b00c0a8adac20557db2cc96a
Ryan Dens <ryandens735@gmail.com>
2019-08-04 19:55:15 -0400

    :sparkles: implement method which gets users from a local csv file

--- user.go
+++ user.go
@@ -1,10 +1,42 @@
 package main
 
+import (
+	"bufio"
+	"encoding/csv"
+	"io"
+	"log"
+	"os"
+)
+
 type User struct {
 	Name           string
 	HashedPassword string
 }
 
+// read existing users into memory
+// adapted from example provided in https://golang.org/pkg/encoding/csv/
 func GetUsers(fileName string) ([]User, error) {
-	return nil, nil
+	file, e := os.Open(fileName)
+	if e != nil {
+		log.Fatal("Problem opening users.csv")
+		return nil, e
+	}
+
+	reader := csv.NewReader(bufio.NewReader(file))
+	var users []User
+
+	for {
+		line, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Problem reading line with error %v", err)
+		}
+		if len(line) != 2 {
+			log.Fatalf("Encountered CSV entry with invalid number of columns")
+		}
+		users = append(users, User{Name: line[0], HashedPassword: line[1]})
+	}
+	return users, nil
 }

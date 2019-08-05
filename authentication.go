0206b0df4fbc46f171136ed991d03b62841f9315
Ryan Dens <ryandens735@gmail.com>
2019-08-04 21:19:56 -0400

    :sparkles: implement VerifyBasicAuth

--- authentication.go
+++ authentication.go
@@ -1,9 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"golang.org/x/crypto/bcrypt"
+	"log"
+)
 
 // Takes an HTTP request and verifies that the supplied username and password matches an entry in the
 // supplied array of Users
-func VerifyBasicAuth(r *http.Request, users []User) bool {
+func VerifyBasicAuth(suppliedUsername string, suppliedPassword string, ok bool, users []User) bool {
+	suppliedPasswordByteArray := []byte(suppliedPassword)
+
+	if !ok {
+		log.Fatal("Problem parsing Basic Auth header")
+		return false
+	}
+
+	for _, user := range users {
+		if user.Name == suppliedUsername {
+			return nil == bcrypt.CompareHashAndPassword([]byte(user.HashedPassword), suppliedPasswordByteArray)
+		}
+	}
 	return false
 }

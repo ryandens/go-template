ed98730c461bf2e886fa090b5b33afe7244b11f2
Ryan Dens <ryandens735@gmail.com>
2019-08-04 20:16:05 -0400

    :recycle: create users.csv on server start

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +16,13 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", HomeHandler)
-	log.Print("Listening on localhost")
+	log.Print("Listening on https://localhost:8080/")
+	file, e := os.Create("users.csv")
+	if e != nil {
+		log.Fatal(e)
+	} else {
+		log.Printf("Sucessfully created %v", file.Name())
+	}
 	// log the error
 	log.Fatal(http.ListenAndServeTLS(":8080", "public.crt", "private.key", nil))
 }
